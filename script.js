3c3ad170d1f70d4fbba58ace8e66e6cd4b2f0421
liz.cherukalethu <liz.cherukalethu@brandaddition.com>
2021-10-01 09:39:23 +0100

    added js file

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    console.log(rate);
+    var years = document.getElementById("years").value;
+    var curYear = new Date().getFullYear();
+    var finalYear = parseInt(curYear) + parseInt(years);
+
+    if(principal =='') {
+        alert('Please enter a valid amount.');
+        principal.focus();
+        return false;
+    }
+    var amount = (principal*rate*years)/100;
+    document.getElementById('result').innerHTML= 'If you deposit <mark>'+principal+',</mark><br>'+
+    'at an interest rate of <mark>'+rate+'%.</mark><br>'+
+    'You will receive an amount of <mark>'+amount+',</mark><br>'+
+    'in the year <mark>'+finalYear+'</mark><br>';
+
     
 }
         
